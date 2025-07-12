7406e0c1c86e00a4ce1aedd003224aaca2926755
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2025-07-12 15:08:03 +0100

    Create tenant models

--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
 
-# Create your models here.
+class Tenant(TenantMixin):
+    name = models.CharField(max_length=100, unique=True)
+    subdomain = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = 'tenants'
+
+class Tenant(Tenant):
+    id = models.AutoField(primary_key=True)
